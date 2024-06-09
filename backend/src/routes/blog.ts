604e157773e3a5e402fd0cea5cd81f8c6b46b3f9
rohan11202 <s.rohan11202@gmail.com>
2024-06-10 02:30:18 +0530

    added backend + common module

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,145 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { verify } from 'hono/jwt'
+import { createblogInput, updateblogInput } from "@p4nda11202/blogger";
+
+
+export const blogRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string,
+        JWT_SECRET: string,
+    },
+    Variables: {
+        userId: string,
+    }
+}>();
+
+
+blogRouter.use('/*', async (c, next) => {
+
+    const authHeader = c.req.header("authorization") || "";
+    try {
+        const user = await verify(authHeader, c.env.JWT_SECRET);
+
+        if (user) {
+            c.set("userId", user.id);
+            next();
+        } else {
+            c.status(403);
+            return c.json({
+                message: "You are not logged in"
+            })
+        }
+    } catch (e) {
+        c.status(403);
+        return c.json({
+            message: "You are not logged in"
+        })
+    }
+
+})
+
+blogRouter.post('/', async (c) => {
+
+    const body = await c.req.json();
+
+    const { success } = createblogInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs are not correct"
+        })
+    }
+
+    const authorId = c.get("userId");
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const blog = await prisma.post.create({
+        data: {
+            title: body.title,
+            content: body.content,
+            authorId: authorId,
+        }
+    })
+
+    return c.json({
+        id: blog.id
+    })
+})
+
+blogRouter.put('/', async (c) => {
+    const body = await c.req.json();
+    const { success } = updateblogInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs are not correct"
+        })
+    }
+
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const blog = await prisma.post.update({
+        where: {
+            id: body.id
+        },
+        data: {
+            title: body.title,
+            content: body.content,
+        }
+    })
+
+    return c.json({
+        id: blog.id
+    })
+})
+
+blogRouter.get('/:id', async (c) => {
+    const id = await c.req.param("id");
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+        const blog = await prisma.post.findFirst({
+            where: {
+                id: id
+            },
+        })
+
+        return c.json({
+            blog
+        });
+
+    } catch (e) {
+
+        c.status(411);
+        return c.json({
+            message: "Error while fetching blog post"
+        });
+    }
+})
+
+
+//Todo : Add pagination
+blogRouter.get('/bulk', async (c) => {
+
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const blogs = prisma.post.findMany();
+
+    return c.json({
+        blogs
+    })
+
+})
---
+++ common/src/index.ts
@@ -0,0 +1,42 @@
+import z from "zod";
+
+
+//-------------------------------------------------------------
+
+export const signupInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(6),
+    name:z.string().optional()
+})
+
+export type SignUpInput = z.infer<typeof signupInput>
+
+//-------------------------------------------------------------
+
+export const signinInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(6),
+})
+
+export type SignInInput = z.infer<typeof signinInput>
+
+//-------------------------------------------------------------
+
+export const createblogInput = z.object({
+    title:z.string(),
+    content:z.string()
+})
+
+export type CreateBlogInput = z.infer<typeof createblogInput>
+
+//-------------------------------------------------------------
+
+export const updateblogInput = z.object({
+    title:z.string(),
+    content:z.string()
+})
+
+export type UpdateBlogInput = z.infer<typeof updateblogInput>
+
+//-------------------------------------------------------------
+
